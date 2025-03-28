fc8edbc7596b22e742b75b62a7563d4b058e0797
Radosław Dąbrowski <radek.dab@gmail.com>
2025-03-28 18:54:01 +0100

    Fix base URL

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,11 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: process.env.BASE,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  plugins: [vue()],
 });
